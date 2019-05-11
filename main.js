b88c15bbd7a93a086c161e83cbff5895bfa72bce
yaleksandrova <yaleksandrova@yahoo.com>
2019-05-11 16:37:02 -0600

    Add vars

--- main.js
+++ main.js
@@ -29,5 +29,36 @@ function randomNum(min, max) {
   return Math.floor(Math.random()*(max - min)+  min);
 }
 
+//Update name of the player//
+
+  var player1Name = document.querySelector(".player1-name");
+  var player2Name = document.querySelector(".player2-name");
+  var updatePlayer1= document.querySelector(".update-player1");
+  var updatePlayer2 = document.querySelector(".update-player2");
+
+  var player1GuessInput = document.querySelector(".player1-guess-input");
+  var player2GuessInput = document.querySelector(".player2-guess-input");
+
+  var player1GuessOutput = document.querySelector(".player1-guess-output");
+  var player2GuessOutput = document.querySelector(".player2-guess-output");
+
+  var submitGuess = document.querySelector(".submit-guess");
+  var resetGame = document.querySelector(".reset-game");
+  var clearGame = document.querySelector(".clear-gamne");
 
 
+  //Event Listener//
+
+  submitGuess.addEventListener('click', updateNames);
+
+
+
+  // Functions://
+
+  //.textContent=name;//
+
+  function updateNames(playerName) {
+
+// In html update button//
+
+//  <button onclick="updateName" (document.getElementByClass("name").value">name</button>  //
